e0790bbf7caf63984977b3a31bf8d7464646b19c
1moonala <junseomoon9@gmail.com>
2023-05-20 23:08:50 -0400

    Add more scopes to spotify api

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.get("/login", (req, res) => {
   const state = generateRandomString(16);
   // res.cookie(stateKey, state);
 
-  const scope = "user-read-private user-read-email";
+  const scope = "user-read-private playlist-modify-public playlist-modify-private user-read-recently-played";
 
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
