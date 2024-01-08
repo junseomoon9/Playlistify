fa4d62cbdadd176cd72a4b9478df07f2d085872c
1moonala <junseomoon9@gmail.com>
2024-01-07 22:13:50 -0500

    Completed LandingPage

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import "./LandingPage.css";
 
 export const LandingPage = () => {
+  const login = () => {
+    window.location.href="http://localhost:8888/login";
+  }
+
   return (
     <div className="landing-page">
-      <a href="http://localhost:8888/login">Log in to Spotify</a>
+      <h1 className="landing-page-title">Playlistify</h1>
+      <h2 className="lading-page-subtitle">Generate New Music Suggestions Based on Your Top Favorite Artists.</h2>
+      <button className="landing-page-login-button" onClick={login}>Connect to Spotify</button>
     </div>
   );
 };
